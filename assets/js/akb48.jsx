51b9c52db511c2e7f51edc49fb996a57d06e97eb
Christopher Brown <cebrown5597@gmail.com>
2018-05-02 11:09:50 -0400

    Move cards

--- assets/js/akb48.jsx
+++ assets/js/akb48.jsx
@@ -8,51 +8,61 @@ export default function game_init(root) {
   ReactDOM.render(<AKB48Game />, root);
 }
 
-// App state for Hangman is:
-// {
-//    word: String    // the word to be guessed
-//    guesses: String // letters guessed so far
-// }
-//
-// A TodoItem is:
-//   { name: String, done: Bool }
-
-
 class AKB48Game extends React.Component {
   constructor(props) {
     super(props);
+
+    this.selectCard = this.selectCard.bind(this);
+    this.selected = null;
+
     this.state = {
       deck: [],
-      stage: [null, null, null, null, null, null],
+      stage: [null, null, null, null, null],
       event: null,
       hand: ["test1", "test2", "test3", "test4", "test5"]
     };
   }
 
+  selectCard(e) {
+    if (this.selected == e) {
+      this.selected = null;
+    } else {
+      this.selected = e;
+    }
+  }
+
+  moveCard(e) {
+    if (this.selected != null) {
+      this.setState(function(prevState, props) {
+        //TODO make copies
+        prevState.stage[e] = this.state.hand[this.selected];
+        prevState.hand.splice(this.selected, 1);
+        this.selected = null;
+        return {
+          stage: prevState.stage,
+          hand: prevState.hand
+        }
+      });
+    } else {
+      console.log("hie...");
+    }
+  }
+
   render() {
     let hand_render = this.state.hand.map((card, i) =>
-      <div key={"hand_" + i} className={"col-auto" + (i !== 0 && " ml-3")}>
+      <div onClick={() => this.selectCard(i)} key={"hand_" + i} className={"col-auto" + (i !== 0 && " ml-3")}>
         <Card name={this.state.hand[i]} />
+      </div>
+    );
+    let stage_render = this.state.stage.map((spot, i) =>
+      <div onClick={() => this.moveCard(i)} key={"stage_" + i} className={"col-auto" + (i !== 0 && " ml-3")}>
+        <Place filler={this.state.stage[i] || "Stage"} />
       </div>
     );
     return (
       <div>
         <div className="row no-gutters mb-3 justify-content-center">
-          <div className="col-auto mr-3">
-            <Place filler="Stage" />
-          </div>
-          <div className="col-auto mr-3">
-            <Place filler="Stage" />
-          </div>
-          <div className="col-auto mr-3">
-            <Place filler="Stage" />
-          </div>
-          <div className="col-auto mr-3">
-            <Place filler="Stage" />
-          </div>
-          <div className="col-auto">
-            <Place filler="Stage" />
-          </div>
+          {stage_render}
         </div>
         <div className="row no-gutters mb-3 justify-content-center">
           <div className="col-auto mr-3" style={{marginLeft: -50}}>
